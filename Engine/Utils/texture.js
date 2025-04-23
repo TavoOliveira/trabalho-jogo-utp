ea68b03940568e0ff1b7a0bae363a9b75019c43d
otaviooliveira0099 <otaviooliveira0099@gmail.com>
2025-04-23 18:08:13 +0000

    Implemented texture mirroring logic

--- Engine/Utils/texture.js
+++ Engine/Utils/texture.js
@@ -2,6 +2,9 @@ export default class Texture extends Image {
     constructor(src) {
         super();
         this.src = src;
+        this.scale = 1;
+        this.flipX = false;
+        this.flipY = false;
 
         this.loaded = false;
 
@@ -17,17 +20,29 @@ export default class Texture extends Image {
     draw(ctx, x, y, width = this.width, height = this.height, sx = 0, sy = 0, sWidth = this.width, sHeight = this.height) {
         if (!this.loaded) return;
 
+        ctx.save();
         ctx.imageSmoothingEnabled = false;
+
+        const scaleX = this.flipX ? -1 : 1;
+        const scaleY = this.flipY ? -1 : 1;
+
+        ctx.scale(scaleX, scaleY);
+
+        const drawX = this.flipX ? -(x + width * this.scale) : x;
+        const drawY = this.flipY ? -(y + height * this.scale) : y;
+
         ctx.drawImage(
             this,
             sx,
             sy,
             sWidth,
             sHeight,
-            x,
-            y,
-            width,
-            height
+            drawX,
+            drawY,
+            width * this.scale,
+            height * this.scale
         );
+
+        ctx.restore();
     }
 }
