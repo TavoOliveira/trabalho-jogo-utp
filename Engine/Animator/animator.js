35a9650ed5d5c4b10db24f5b1dc10fffe9493fef
otaviooliveira0099 <otaviooliveira0099@gmail.com>
2025-04-23 18:05:55 +0000

    Sets the class that represents an animation

--- Engine/Animator/animator.js
+++ Engine/Animator/animator.js
@@ -1,10 +1,63 @@
+import Texture from "../Utils/texture.js";
+
 export default class Animator {
+    #currentFrame;
+    #elapsedTime;
+    #frameDuration;
 
-    update() {
+    /**
+     * Cria uma nova animação.
+     * @param {string} name - O nome da animação.
+     * @param {Texture} texture - A textura contendo os quadros da animação.
+     * @param {number} width - A largura de cada quadro na animação.
+     * @param {number} height - A altura de cada quadro na animação.
+     * @param {number} startX - A coordenada x do primeiro quadro na textura.
+     * @param {number} startY - A coordenada y do primeiro quadro na textura.
+     * @param {number} count - O número de quadros na animação.
+     * @param {number} fps - Os quadros por segundo da animação.
+     */
+    constructor(name, texture, width, height, startX, startY, count, fps) {
+        this.name = name;
+        this.texture = texture;
+        this.width = width;
+        this.height = height;
+        this.startX = startX;
+        this.startY = startY;
+        this.count = count;
 
+        this.isPlaying = false;
+
+        this.#currentFrame = 0;
+        this.#elapsedTime = 0;
+        this.#frameDuration = 1000 / fps;
     }
 
-    draw(ctx) {
+    play() {
+        this.isPlaying = true;
+    }
 
+    stop() {
+        this.isPlaying = false;
+        this.#currentFrame = 0;
+        this.#elapsedTime = 0;
+    }
+
+    update(deltaTime) {
+        if (!this.isPlaying) return;
+
+        this.#elapsedTime += deltaTime;
+        if (this.#elapsedTime >= this.#frameDuration) {
+            this.#currentFrame = (this.#currentFrame + 1) % this.count;
+            this.#elapsedTime -= this.#frameDuration;
+        }
+    }
+
+    draw(ctx, position) {
+        if (!this.isPlaying) return;
+
+        const sx = this.startX + this.#currentFrame * this.width;
+        const sy = this.startY;
+
+        this.texture.draw(ctx, position.x, position.y, this.width, this.height, sx, sy, this.width, this.height);
     }
 }
