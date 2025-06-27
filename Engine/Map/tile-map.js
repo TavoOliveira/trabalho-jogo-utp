ed3adc9cc8dc1f91aaf07dd2f8f9e7ed1740a8fb
TavoOliveira <otaviooliveira0099@gmail.com>
2025-06-27 21:11:05 +0000

    Add TileMap

--- Engine/Map/tile-map.js
+++ Engine/Map/tile-map.js
@@ -2,21 +2,29 @@ import Texture from "../Utils/texture.js";
 
 export default class TileMap {
     constructor(mapData) {
-        this.tileSize = mapData.tileSize;
-        this.tileset = new Texture(mapData.tileset);
-        this.tileDefinitions = mapData.tileDefinitions;
-        this.tiles = mapData.tiles;
-        this.width = this.tiles[0].length;
-        this.height = this.tiles.length;
+        console.log(mapData.Tiles[0][0].length);
+
+        this.tileSize = mapData.TileSize;
+        this.tileset = new Texture(mapData.TileSet);
+        this.tileDefinitions = mapData.TileDefinitions;
+        this.tiles = mapData.Tiles;
+
+        // Calcula largura e altura a partir da primeira camada
+        this.layerCount = this.tiles.length;
+        this.height = this.tiles[0].length;
+        this.width = this.tiles[0][0].length;
     }
 
     draw(ctx) {
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                const tileId = this.tiles[y][x];
+        for (let layer = 0; layer < this.layerCount; layer++) {
+            for (let y = 0; y < this.height; y++) {
+                for (let x = 0; x < this.width; x++) {
+                    const tileId = this.tiles[layer][y][x];
 
-                if (this.tileDefinitions[tileId]) {
-                    const { sx, sy } = this.tileDefinitions[tileId];
+                    // Ignora null ou indefinido
+                    if (tileId == null || this.tileDefinitions[tileId] == null) continue;
+
+                    const { Sx, Sy } = this.tileDefinitions[tileId];
 
                     this.tileset.draw(
                         ctx,
@@ -24,8 +32,8 @@ export default class TileMap {
                         y * this.tileSize,
                         this.tileSize,
                         this.tileSize,
-                        sx,
-                        sy,
+                        Sx * this.tileSize,
+                        Sy * this.tileSize,
                         this.tileSize,
                         this.tileSize
                     );
@@ -33,5 +41,4 @@ export default class TileMap {
             }
         }
     }
-
 }
